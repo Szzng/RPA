6c0bcf65820ea3e6afb3d25ad85dddb5dc28d152
Szzng <66233687+Szzng@users.noreply.github.com>
2023-07-04 14:27:04 +0900

    fix: remove header

--- score/main.py
+++ score/main.py
@@ -3,7 +3,7 @@ import random
 
 data = 'data.xlsx'
 subjects = ['국어', '사회', '도덕', '수학', '과학', '체육', '미술']
-subjectsDict = pd.read_excel(data, engine='openpyxl', sheet_name=None)
+subjectsDict = pd.read_excel(data, engine='openpyxl', sheet_name=None, header=None)
 for i in subjectsDict:
   subjectsDict[i] = subjectsDict[i].dropna()
 
@@ -17,7 +17,7 @@ for subject in subjects:
         if len(value) > 0:
             temp.append(value[0])
 
-    index[subject] = [0, ]
+    index[subject] = []
     for i in range(len(temp)):
         if '[' in temp[i]:
             index[subject].append(i)
@@ -27,7 +27,8 @@ for subject in subjects:
         globals()["temp{}".format(i)] = temp[index[subject][i] + 1:index[subject][i + 1]]
 
     finalDict[subject] = []
-    for i in range(30):
+    students_num= 35
+    for i in range(students_num):
         sentence = ''
         for j in range(len(index[subject]) - 1):
             sentence += random.choice(globals()["temp{}".format(j)]) + ' '
