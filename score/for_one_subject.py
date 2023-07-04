828c563b5a7bd4b12f80533830a1e1ebd20fe33e
Szzng <66233687+Szzng@users.noreply.github.com>
2023-07-04 14:52:25 +0900

    feat: 교과실용 - 1인 1엑셀 파일, 7반*35명 세트

---
+++ score/for_one_subject.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import random
+
+data = 'data.xlsx'
+subjects = ['영어5', '과학5', '음악6']
+subjectsDict = pd.read_excel(data, engine='openpyxl', sheet_name=None, header=None)
+for i in subjectsDict:
+  subjectsDict[i] = subjectsDict[i].dropna()
+
+index = {}
+finalDict = {}
+
+for subject in subjects:
+    for num in range(1, 8):
+      temp = []
+
+      for value in subjectsDict[subject].values:
+          if len(value) > 0:
+              temp.append(value[0])
+
+      index[subject] = []
+      for i in range(len(temp)):
+          if '[' in temp[i]:
+              index[subject].append(i)
+      index[subject].append(len(temp))
+
+      for i in range(len(index[subject]) - 1):
+          globals()["temp{}".format(i)] = temp[index[subject][i] + 1:index[subject][i + 1]]
+
+      finalDict[num] = []
+      students_num= 35
+      for i in range(students_num):
+          sentence = ''
+          for j in range(len(index[subject]) - 1):
+              sentence += random.choice(globals()["temp{}".format(j)]) + ' '
+              sentence = sentence.replace('\t', '')
+          finalDict[num].append(sentence)
+
+    df = pd.DataFrame(finalDict)
+    df.to_excel(f'final{subject}.xlsx')
