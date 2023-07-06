9f4623a4b6021da7ce16abb85181ae8e8d8cff1a
Szzng <66233687+Szzng@users.noreply.github.com>
2023-07-06 13:06:51 +0900

    refactor: clean code

--- score/for_one_subject.py
+++ score/for_one_subject.py
@@ -2,13 +2,13 @@ import pandas as pd
 import random
 
 data = 'data.xlsx'
-subjects = ['영어5', '과학5', '음악6']
 subjectsDict = pd.read_excel(data, engine='openpyxl', sheet_name=None, header=None)
 for i in subjectsDict:
   subjectsDict[i] = subjectsDict[i].dropna()
 
 index = {}
 finalDict = {}
+subjects = list(subjectsDict.keys())
 
 for subject in subjects:
     for num in range(1, 8):
@@ -32,7 +32,7 @@ for subject in subjects:
       for i in range(students_num):
           sentence = ''
           for j in range(len(index[subject]) - 1):
-              sentence += random.choice(globals()["temp{}".format(j)]) + ' '
+              sentence += random.choice(globals()["temp{}".format(j)]).strip() + ' '
               sentence = sentence.replace('\t', '')
           sentence = sentence.strip()
           finalDict[num].append(sentence)
--- score/main.py
+++ score/main.py
@@ -2,13 +2,13 @@ import pandas as pd
 import random
 
 data = 'data.xlsx'
-subjects = ['국어', '사회', '도덕', '수학', '과학', '체육', '미술']
 subjectsDict = pd.read_excel(data, engine='openpyxl', sheet_name=None, header=None)
 for i in subjectsDict:
   subjectsDict[i] = subjectsDict[i].dropna()
 
 index = {}
 finalDict = {}
+subjects = list(subjectsDict.keys())
 
 for subject in subjects:
     temp = []
@@ -31,7 +31,7 @@ for subject in subjects:
     for i in range(students_num):
         sentence = ''
         for j in range(len(index[subject]) - 1):
-            sentence += random.choice(globals()["temp{}".format(j)]) + ' '
+            sentence += random.choice(globals()["temp{}".format(j)]).strip() + ' '
             sentence = sentence.replace('\t', '')
         sentence = sentence.strip()
         finalDict[subject].append(sentence)
